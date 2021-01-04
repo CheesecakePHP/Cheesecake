94bb2545d4067170845ad9012633ff42032081ae
Christian Meyer <ak56lk@gmx.net>
2021-01-04 01:37:02 +0100

    no message

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
 <?php
+
+use Cheesecake\Http\Response;
+
 require_once(rtrim($_SERVER['DOCUMENT_ROOT'], '/') .'/vendor/autoload.php');
 
-use Cheesecake\Crust;
-use Cheesecake\Exception\ControllerNotExistsException;
-use Cheesecake\Exception\MethodNotExistsException;
-use Cheesecake\Exception\RouteNotDefinedException;
-use Cheesecake\Http\Request;
-use Cheesecake\Http\Response;
-use Cheesecake\Routing\Router;
-
-try {
-    $dir = new DirectoryIterator('src/routes/');
-
-    foreach ($dir as $fileinfo) {
-        if (!$fileinfo->isDot()) {
-            include_once($fileinfo->getPathname());
-        }
-    }
-
-    $CheesecakeCrust = new Crust(Router::route(Request::requestMethod(), Request::requestUri()));
-
-    Response::sendHeader(Response::HTTP_STATUS_200_OK);
-
-    $result = $CheesecakeCrust->run();
-}
-catch (ControllerNotExistsException | MethodNotExistsException | RouteNotDefinedException $e) {
-    Response::sendHeader(Response::HTTP_STATUS_404_NOT_FOUND);
-
-    $result = [
-        'error' => [
-            'code' => 404,
-            'message' => 'Not Found'
-        ]
-    ];
-}
-catch (Exception $e) {
-    Response::sendHeader(Response::HTTP_STATUS_500_INTERNAL_SERVER_ERROR);
-
-    $result = [
-        'error' => [
-            'code' => $e->getCode() . '-root',
-            'message' => $e->getMessage()
-        ]
-    ];
-}
+$Crust = require_once('src/bootstrap.php');
+$result = $Crust->run();
 
 Response::sendHeader(Response::HPPT_CONTENT_TYPE_JSON);
-
 echo json_encode($result);
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,12 +1,1 @@
 <?php
-
-Router::get('ping', 'Test@ping');
-
-Router::group([
-    'prefix' => 'v1/',
-    'middleware' => \App\Middleware\Auth::class
-], function() {
-    Router::get('user/{id}', 'User@get');
-    Router::get('user/{id}/edit', 'User@edit');
-    Router::get('user/{id}/edit/{identifier}', 'User@edit');
-});
